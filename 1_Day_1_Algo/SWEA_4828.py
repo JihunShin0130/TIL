3067c00c385302b96f61cf338f9b5d174a589a62
JihunShin0130 <sjhoon130@gmail.com>
2022-02-17 23:18:39 +0900

    SWEA Algorithm 4828번 문제

--- 1_Day_1_Algo/SWEA_4828.py
+++ 1_Day_1_Algo/SWEA_4828.py
@@ -4,5 +4,4 @@ for test_case in range(1, T + 1):
     a=int(input())
     b=list(map(int,input().split()))
     #print("#{} {}".format(test_case,max(b)-min(b)))
-    #f-string 활용
     print(f"#{test_case} {max(b)-min(b)}") 
